7bcd9a6970ac2da37e7b87556cac9193e0bdd3f1
vnvnsairam <vnvnsairam@gmail.com>
2014-05-15 02:51:51 +0000

    removed sudo node from server.sh

--- server.sh
+++ server.sh
@@ -2,4 +2,4 @@
 
 epdc-app  -start_epdc 0 1
 sudo python /home/PL_web-app/kill_port.py
-sudo /root/.nvm/versions/node/v4.8.4/bin/node /home/PL_web-app/server.js  > /home/PL_web-app/src/logs/webapplication.log 2>&1
+node /home/PL_web-app/server.js  > /home/PL_web-app/src/logs/webapplication.log 2>&1

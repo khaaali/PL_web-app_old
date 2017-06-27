80960e169f5e56b1da0c3d1121e21b6aaf800c1f
Sairam Vankamamidi <sairam.vankamamidi@plasticlogic.com>
2017-06-27 14:14:18 +0200

    addded upload page and works

--- server.js
+++ server.js
@@ -3,45 +3,166 @@
 
 var express = require("express"),
     app = express(),
-    formidable = require('formidable'),
+    //formidable = require('formidable'),
     util = require('util'),
-    fs   = require('fs-extra'),
-    qt   = require('quickthumb'),
+    fs = require('fs-extra'),
+    //qt   = require('quickthumb'),
     path = require('path'),
-    port=3003;
+    multer = require('multer'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    port = 3003;
+
+app.use(express.static(path.join(__dirname, 'src')));
+
 var imageDir = "C:\\Users\\sairam.vankamamidi\\Documents\\PL_web-app\\new\\src\\assets\\demo_images\\";
+
+var storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, "src/assets/demo_images/");
+    },
+    filename: function(req, file, callback) {
+        console.log(file.mimetype)
+        if (file.mimetype == 'image/png') {
+            callback(null, file.originalname);
+        } else {
+            callback(new Error('I don\'t have a clue!'));
+        }
+    }
+});
+
+var upload = multer({ storage: storage }).any();
+
+
 
 //app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'src')));
-
-
 //app.use('/', require('./routes/index')); //added
 
 
 
-app.get("/",function(req,res){
-        
-      res.sendFile(__dirname+'/src/index.html');
+app.get("/", function(req, res) {
+
+    res.sendFile(__dirname + '/src/index.html');
 });
 
-app.get("/home",function(req,res){
-        
-      res.sendFile(__dirname+'/src/index.html');
+app.get("/home", function(req, res) {
+
+    res.sendFile(__dirname + '/src/index.html');
 });
 
-app.get("/upload_image",function(req,res){
-  res.sendFile(__dirname+'/src/upload_image.html');
-});
-
-
-app.get("/displayimagee",function(req,res){
-  res.sendFile(__dirname+'/src/display_img.html');
-    
+app.get("/upload_image", function(req, res) {
+    res.sendFile(__dirname + '/src/upload_image.html');
 });
 
 
-app.post('/upload', function (req, res){
+app.get("/displayimage", function(req, res) {
+    res.sendFile(__dirname + '/src/display_img2.html');
+
+});
+
+
+app.delete("/delete/:id", function(req, res) {
+
+    var id = req.params.id
+    console.log("Got a DELETE request for", id);
+    fs.unlink(imageDir + id)
+    console.log(imageDir + id);
+    res.send('Hello DELETE');
+});
+
+
+app.get("/upload_showImageList", function(req, res, next) {
+    //res.setHeader("Content-Type", "text/html");
+    getImages(imageDir, function(err, files) {
+        var imageLists = [];
+        for (var i = 0; i < files.length; i++) {
+            imageLists.push(files[i]);
+        }
+        console.log(files.length);
+        console.log(imageLists);
+        res.json(imageLists);
+    });
+});
+
+
+
+app.get("/displayimage/:imageId", function(req, res) {
+    //res.setHeader("Content-Type", "text/html");
+    console.log(req.params.imageId);
+    var id = req.params.imageId;
+    res.end(id);
+});
+
+
+
+app.post('/upload', function(req, res, next) {
+    res.setHeader("Content-Type", "text/html");
+    upload(req, res, function(err) {
+        console.log(req.files);
+        if (err) {
+            res.sendFile(__dirname + '/src/upload_image.html');
+            console.log('not supported format')
+        }
+        res.sendFile(__dirname + '/src/upload_image.html');
+        console.log('supported format')
+    });
+    res.sendFile(__dirname + '/src/upload_image.html');
+})
+
+
+
+app.get("*", function(req, res) {
+
+    res.sendFile(__dirname + '/src/index.html');
+});
+
+app.listen(port);
+
+console.log('Listening on localhost port ' + port);
+
+module.exports = app; //added
+
+
+
+
+
+// filter to show only png from demo_images folder in html page
+function getImages(imageDir, callback) {
+    var fileType = '.png',
+        files = [],
+        i;
+    fs.readdir(imageDir, function(err, list) {
+        for (i = 0; i < list.length; i++) {
+            if (path.extname(list[i]) === fileType) {
+                files.push(list[i]); //store the file name into the array files
+            }
+        }
+        callback(err, files);
+    });
+}
+
+
+function redirectRouterUploadPage(req, res) {
+    res.sendFile(__dirname + '/src/upload_image.html');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+app.post('/uploads', function (req, res){
   var form = new formidable.IncomingForm();
   form.parse(req, function(err, fields, files) {
   //  res.writeHead(200, {'content-type': 'text/plain'});
@@ -51,13 +172,13 @@ app.post('/upload', function (req, res){
   });
 
   form.on('end', function(fields, files) {
-    /* Temporary location of our uploaded file */
+    Temporary location of our uploaded file 
     var temp_path = this.openedFiles[0].path;
-    /* The file name of the uploaded file */
+    The file name of the uploaded file 
     console.log(temp_path)
 
     var file_name = this.openedFiles[0].name;
-    /* Location where we want to copy the uploaded file */
+    Location where we want to copy the uploaded file
     var new_location = imageDir ;
 
     fs.copy(temp_path, new_location + file_name, function(err) {  
@@ -65,10 +186,25 @@ app.post('/upload', function (req, res){
         console.error(err);
       } else {
         console.log("success!")
+        res.sendFile(__dirname+'/src/upload_image.html');
       }
     });
   });
+
 });
+
+
+app.post('/uploaad',function(req,res){
+    upload(req,res,function(err) {
+        console.log(req.body);
+        console.log(req.files);
+        if(err) {
+            return res.end("Error uploading file.");
+        }
+        res.end("File is uploaded");
+    });
+});
+
 
 
 app.get("/display_image",function(req,res){
@@ -88,72 +224,12 @@ app.get("/display_image",function(req,res){
 });
 
 
-app.get("/displayimage",function(req,res,next){
-    getImages(imageDir, function (err, files) {
-            var imageLists=[];
-            for (var i=0; i<files.length; i++) {
-                imageLists +=  files[i] ;
-            }
-            console.log(imageLists);
-            res.end(imageLists);
-
-});
-
-});
-
-app.get('/test', function(req, res, next) {
-  res.json('Hello World');
-});
-
-
- 
-app.get("/display_image/:imageId",function(req,res){
-    console.log(req.params.imageId);
+app.get("/displayimagee",function(req,res){
+  res.sendFile(__dirname+'/src/display_img.html');
+    
 });
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//function redirectRouter(req,res){
-//  res.sendFile(__dirname+'/src/index.html');
-//}
-
-//app.use(redirectRouter);
-
-app.listen(port);
-
-console.log('Listening on localhost port '+port);
-
-module.exports = app;//added
-
-
-function getImages(imageDir, callback) {
-    var fileType = '.png',
-        files = [], i;
-    fs.readdir(imageDir, function (err, list) {
-        for(i=0; i<list.length; i++) {
-            if(path.extname(list[i]) === fileType) {
-                files.push(list[i]); //store the file name into the array files
-            }
-        }
-        callback(err, files);
-    });
-}
+*/
